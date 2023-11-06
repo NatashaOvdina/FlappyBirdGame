fb2d5d61c55f87c61e4103eb7d53366ed08a7a16
NatashaOvdina <natashaovdina@yahoo.com>
2023-11-06 20:07:58 +0300

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by Natalia Ovdina on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
